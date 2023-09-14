7e3fabc9880283b21903363f859afe69594553e3
Benjamin Swart <Benjaminswart@email.cz>
2023-09-14 15:02:26 +0200

    Convert path segments to camel case

--- throwing-macros/src/names.rs
+++ throwing-macros/src/names.rs
@@ -20,24 +20,23 @@ fn path_to_variant(path: &Path) -> Ident {
     let mut name = String::new();
 
     for segment in &path.segments {
-        let segment = segment.ident.to_string();
-        let mut chars = segment.chars();
-
-        if let Some(c) = chars.next() {
-            name.extend(c.to_uppercase());
-        }
-
-        name.extend(chars);
+        snake_case_to_camel_case(&segment.ident, &mut name);
     }
 
     Ident::new(&name, Span::mixed_site())
 }
 
 pub fn fn_name_to_error(ident: &Ident) -> Ident {
-    let name = ident.to_string();
-    let segments = name.split('_');
-
     let mut name = String::new();
+    snake_case_to_camel_case(ident, &mut name);
+    name.push_str("Error");
+
+    Ident::new(&name, Span::mixed_site())
+}
+
+pub fn snake_case_to_camel_case(ident: &Ident, name: &mut String) {
+    let ident = ident.to_string();
+    let segments = ident.split('_');
 
     for segment in segments {
         let mut chars = segment.chars();
@@ -48,8 +47,4 @@ pub fn fn_name_to_error(ident: &Ident) -> Ident {
 
         name.extend(chars);
     }
-
-    name.push_str("Error");
-
-    Ident::new(&name, Span::mixed_site())
 }
